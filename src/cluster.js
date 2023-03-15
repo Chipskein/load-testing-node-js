87ca85d5ab1bebd032a83e5ff492d0b78b9a173a
Chipskein <abfn0905@gmail.com>
2023-03-14 21:37:58 -0300

    init

--- src/cluster.js
+++ src/cluster.js
@@ -6,6 +6,15 @@ if(cluster.isMaster){
     for(let c=0;c<cpus;c++){
         cluster.fork()
     }
+    cluster.on('online', function(worker) {
+        console.log('worker:', worker.process.pid, 'is up');
+    })
+    cluster.on('exit', (worker, code, signal) => {
+        if(code !== 0 && !worker.exitedAfterDisconnect) {
+            console.log('Starting new worker:', worker.process.pid, + 'signal:', signal);
+            cluster.fork();
+        }
+    })
 }
 else{
     app.Init()
